65e1686e82f82e2cab2277e3555c6b104478eb08
Warley-Juneo <warleyjuneo111@gmail.com>
2022-05-06 20:18:29 -0300

    Finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+import { CorrentistaComponent } from './components/correntista/correntista.component';
+//
+const routes: Routes = [
+  // Colando as rotas //
+  { path: 'movimentacoes-new', component: MovimentacaoNewComponent },
+  { path: 'movimentacoes', component: MovimentacaoListComponent },
+  { path: 'correntistas', component: CorrentistaComponent },
+  { path: '', redirectTo: 'movimentacoes', pathMatch: 'full' },
+  //
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
